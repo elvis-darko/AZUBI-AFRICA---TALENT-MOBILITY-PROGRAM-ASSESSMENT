1088f5ed7aac674b8a2de665e98757ece3f126a1
Elvis DARKO <elvis_darko@outlook.com>
2024-12-12 11:16:27 +0000

    Update streamlit.py

--- ASSETS/src/streamlit.py
+++ ASSETS/src/streamlit.py
@@ -63,7 +63,9 @@ def prediction_page():
     # Raw GitHub URL of your model
     #gb_model_tund = joblib.load(r"C:\\Users\\ICUMS\\Documents\\GitHub\\AZUBI-AFRICA---TALENT-MOBILITY-PROGRAM-ASSESSMENT\\ASSETS\\src\\gb_model_tuned.joblib")
 
-    gb_model_tuned = "https://github.com/elvis-darko/AZUBI-AFRICA---TALENT-MOBILITY-PROGRAM-ASSESSMENT/raw/main/ASSETS/src/gb_model_tuned.joblib"
+    #gb_model_tuned = "https://github.com/elvis-darko/AZUBI-AFRICA---TALENT-MOBILITY-PROGRAM-ASSESSMENT/raw/main/ASSETS/src/gb_model_tuned.joblib"
+    model = joblib.load("gb_model_tuned.joblib")
+
     # Download the model file from the URL and save it locally
     # response = requests.get(model_url)
     # if response.status_code == 200:
@@ -169,7 +171,7 @@ def prediction_page():
         #st.dataframe([input_features])
         #gb_model_tuned = joblib.load(r"C:\\Users\\ICUMS\\Documents\\GitHub\\AZUBI-AFRICA---TALENT-MOBILITY-PROGRAM-ASSESSMENT\\ASSETS\\src\\gb_model_tuned.joblib")
 
-        prediction = gb_model_tuned.predict([input_features])
+        prediction = model.predict([input_features])
         #prediction_probability = gb_model_tuned.predict_proba(input_features)[:, 1]  # Probability of churn
 
         if prediction[0] == "yes":
