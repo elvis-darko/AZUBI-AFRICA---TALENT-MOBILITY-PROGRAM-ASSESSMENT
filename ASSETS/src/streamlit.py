9de977fd348c78a7c56c20cdb41a21104cc7ed4c
Elvis DARKO <elvis_darko@outlook.com>
2024-12-13 10:54:30 +0000

    Update streamlit.py

--- ASSETS/src/streamlit.py
+++ ASSETS/src/streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
 # import requests
-import numpy
+#import numpy
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import pickle
