a5bd9c9c583f31fd2a93cbab873ca2c06c16c891
Elvis DARKO <elvis_darko@outlook.com>
2024-12-16 13:21:57 +0000

    Update streamlit.app.py

--- ASSETS/src/streamlit.app.py
+++ ASSETS/src/streamlit.app.py
@@ -155,9 +155,9 @@ def prediction_page():
         # Download the model file from the URL and save it locally
         response = requests.get(model_url)
         if response.status_code == 200:
-        with open("gb_model_tuned.joblib", "wb") as f:
-            f.write(response.content)
-        gb_model_tuned = pickle.load("gb_model_tuned.pkl")
+            with open("gb_model_tuned.joblib", "wb") as f:
+                f.write(response.content)
+            gb_model_tuned = pickle.load("gb_model_tuned.pkl")
         else:
              st.error("Failed to load the model from GitHub.")
         
